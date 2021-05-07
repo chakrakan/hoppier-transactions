7b867c5bfe45546eaa2dbf57e866609a7cd2a7ec
Kanisk Chakraborty <57884127+chakrakan@users.noreply.github.com>
2021-05-07 17:01:50 -0400

    switch deprecated conversion api

--- src/utils/FetchCurrency.js
+++ src/utils/FetchCurrency.js
@@ -1,21 +1,12 @@
-export const fetchCurrency = async () => {
-  const rateUSDtoCAD = await fetch(
-    'https://api.exchangeratesapi.io/latest?base=USD&symbols=CAD'
-  )
-    .then((res) => res.json())
-    .then((data) => {
-      return data.rates.CAD;
-    })
-    .catch((err) => console.log(err));
+import { exchangeRates } from 'exchange-rates-api';
 
-  //   const rateCADtoUSD = await fetch(
-  //     'https://api.exchangeratesapi.io/latest?base=CAD&symbols=USD'
-  //   )
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       return data.rates.USD;
-  //     })
-  //     .catch((err) => console.log(err));
+export const fetchCurrency = async () => {
+  const rateUSDtoCAD = await exchangeRates()
+    .latest()
+    .base('USD')
+    .fetch()
+    .then((res) => res.CAD)
+    .catch((err) => console.log(err));
 
   return { rateUSDtoCAD };
 };
